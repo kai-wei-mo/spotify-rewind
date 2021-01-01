f1f532ed707dc75241ef5727cab2f257bce4804a
Kai Wei Mo <devinkwmo@gmail.com>
2021-01-01 02:39:33 -0500

    Added front-end for playlist generation.

--- public/services/createPlaylist.js
+++ public/services/createPlaylist.js
@@ -1,56 +1,60 @@
 // RETURNS A PLAYLIST ENDPOINT
 
-function createPlaylist(playlist_name="New Playlist by Spotify Rewind", public=false, collaborative=false, description='') {
+function createPlaylist(
+	playlist_name = 'New Playlist by Spotify Rewind',
+	public = false, // permissions for public playlists are not requested in this version
+	collaborative = false,
+	description = ''
+) {
+	/**
+	 * Obtains parameters from the hash of the URL
+	 * @return Object
+	 */
+	function getHashParams() {
+		var hashParams = {};
+		var e,
+			r = /([^&;=]+)=?([^&;]*)/g,
+			q = window.location.hash.substring(1);
+		while ((e = r.exec(q))) {
+			hashParams[e[1]] = decodeURIComponent(e[2]);
+		}
+		return hashParams;
+	}
 
-    /**
-     * Obtains parameters from the hash of the URL
-     * @return Object
-     */
-    function getHashParams() {
-      var hashParams = {};
-      var e, r = /([^&;=]+)=?([^&;]*)/g,
-          q = window.location.hash.substring(1);
-      while ( e = r.exec(q)) {
-         hashParams[e[1]] = decodeURIComponent(e[2]);
-      }
-      return hashParams;
-    }
+	var params = getHashParams();
+	var error = params.error;
+	let access_token = sessionStorage.getItem('access_token');
+	let user_id = sessionStorage.getItem('user_id');
 
-    var params = getHashParams();
-
-    var error = params.error;
-    let access_token = sessionStorage.getItem("access_token");
-
-    let user_id = sessionStorage.getItem("user_id");
-
-    if (error) {
-      alert('There was an error during the authentication.');
-    } else {
-      if (access_token) {
-
-        $.ajax({
-            type: 'POST',
-            url: `https://api.spotify.com/v1/users/${user_id}/playlists`,
-            headers: {
-              'Authorization': 'Bearer ' + access_token,
-              'Content-Type': 'application/json'
-            },
-            data: JSON.stringify({
-              "name": playlist_name,
-              "public": false, // permissions for public playlists are not requested in this version
-              "collaborative": collaborative,
-              "description": description
-            }),
-            success: function(response) {
-            sessionStorage.setItem("playlist_id", response.id);
-            },
-            error: function() {
-              window.location.href = 'Playlist generation is not available at this time.\nPlease try again later.';
-            }
-        });
-      } else {
-          alert("A change of state has occured.\nPlease log in again.");
-          window.location.href = '/';
-      }
-    }
-  };
+	if (error) {
+		alert('There was an error during the authentication.');
+	} else {
+		if (access_token) {
+			$.ajax({
+				type: 'POST',
+				url: `https://api.spotify.com/v1/users/${user_id}/playlists`,
+				headers: {
+					Authorization: 'Bearer ' + access_token,
+					'Content-Type': 'application/json',
+				},
+				data: JSON.stringify({
+					name: playlist_name,
+					public: public,
+					collaborative: collaborative,
+					description: description,
+				}),
+				success: function (response) {
+					sessionStorage.setItem('playlist_id', response.id);
+				},
+				error: function () {
+					alert(
+						'Playlist generation is not available at this time.\nPlease try again later.'
+					);
+				},
+			});
+		} else {
+			alert('A change of state has occured.\nPlease log in again.');
+			window.location.href = '/';
+		}
+	}
+}
--- public/services/populatePlaylist.js
+++ public/services/populatePlaylist.js
@@ -1,58 +1,55 @@
 function populatePlaylist() {
+	/**
+	 * Obtains parameters from the hash of the URL
+	 * @return Object
+	 */
+	function getHashParams() {
+		var hashParams = {};
+		var e,
+			r = /([^&;=]+)=?([^&;]*)/g,
+			q = window.location.hash.substring(1);
+		while ((e = r.exec(q))) {
+			hashParams[e[1]] = decodeURIComponent(e[2]);
+		}
+		return hashParams;
+	}
 
-    /**
-     * Obtains parameters from the hash of the URL
-     * @return Object
-     */
-    function getHashParams() {
-      var hashParams = {};
-      var e, r = /([^&;=]+)=?([^&;]*)/g,
-          q = window.location.hash.substring(1);
-      while ( e = r.exec(q)) {
-         hashParams[e[1]] = decodeURIComponent(e[2]);
-      }
-      return hashParams;
-    }
+	var params = getHashParams();
+	var error = params.error;
+	let access_token = sessionStorage.getItem('access_token');
+	let playlist_id = sessionStorage.getItem('playlist_id');
 
-    var params = getHashParams();
+	if (error) {
+		alert('There was an error during the authentication.');
+	} else {
+		if (access_token) {
+			let selected_links = document.querySelectorAll('.track');
+			let uri_arr = [];
 
-    var error = params.error;
-    let access_token = sessionStorage.getItem("access_token");
-    let playlist_id = sessionStorage.getItem("playlist_id");
+			for (let i = 0; i < selected_links.length; i++) {
+				uri_arr[i] = selected_links[i].href;
+			}
 
-    if (error) {
-      alert('There was an error during the authentication.');
-    } else {
-      if (access_token) {
-
-        let selected_links = document.querySelectorAll('.track');
-        let uri_arr = [];
-
-        for (let i = 0; i < selected_links.length; i++) {
-          uri_arr[i] = selected_links[i].href;
-        }
-
-        $.ajax({
-            type: 'POST',
-            url: `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`,
-            headers: {
-              'Authorization': 'Bearer ' + access_token,
-              'Content-Type': 'application/json'
-            },
-            data: JSON.stringify({
-              "uris": uri_arr
-            }),
-            success: function(response) {
-              sessionStorage.setItem("playlist_id", '-1');
-            },
-            error: function() {
-              console.log(playlist_id);
-              alert('An unexpected error has occured.');
-            }
-        });
-      } else {
-          alert("A change of state has occured.\nPlease log in again.");
-          window.location.href = '/';
-      }
-    }
-  };
+			$.ajax({
+				type: 'POST',
+				url: `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`,
+				headers: {
+					Authorization: 'Bearer ' + access_token,
+					'Content-Type': 'application/json',
+				},
+				data: JSON.stringify({
+					uris: uri_arr,
+				}),
+				success: function (response) {
+					sessionStorage.destroyItem('playlist_id');
+				},
+				error: function () {
+					alert('An unexpected error has occured.');
+				},
+			});
+		} else {
+			alert('A change of state has occured.\nPlease log in again.');
+			window.location.href = '/';
+		}
+	}
+}
