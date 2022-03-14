d91d5d75f17097b7b89759b2d56e4c907864983e
Kai Wei Mo <devinkwmo@gmail.com>
2022-03-13 22:33:09 -0400

    Nuke everything

--- public/services/destroySession.js
+++
@@ -1,4 +0,0 @@
-function destroySession() {
-	sessionStorage.removeItem('access_token');
-	sessionStorage.removeItem('refresh_token');
-}
--- public/services/redirectIfUnauth.js
+++
@@ -1,9 +0,0 @@
-/**
- * If the client is unauthenticated, redirect them to the login page.
- * This also accounts for legitimate users who have simply expired their
- * session.
- */
-
-if (!sessionStorage.getItem('access_token')) {
-	window.location.assign('/');
-}
