5544895ea844fbaa6e7ecab864b986752b23039f
Kai Wei Mo <devinkwmo@gmail.com>
2022-03-15 10:19:25 -0400

    Add /me

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,48 @@
 import { useState, useEffect } from 'react';
-import { accessToken, logout } from './spotify';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
+import { catchErrors } from './utils';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {!token && <a className="App-link" href="http://localhost:8888/login">Login</a>}
-        {token && (<> <p>Logged in</p> <button onClick={logout}>Log out</button> </>)}
+        {!token ? (
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <>
+            <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Avatar" />
+                )}
+              </div>
+            )}
+          </>
+        )}
       </header>
-    </div >
+    </div>
   );
 }
 
