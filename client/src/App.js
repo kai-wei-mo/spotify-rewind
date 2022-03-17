18296d5f38a74710f73efae1d69843f811145a68
Kai Wei Mo <devinkwmo@gmail.com>
2022-03-16 20:24:02 -0400

    Add routing with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,25 @@ import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = useState(null);
@@ -27,19 +46,35 @@ function App() {
             Log in to Spotify
           </a>
         ) : (
-          <>
-            <button onClick={logout}>Log Out</button>
+          <Router>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/top-artists" element={<h1>Top Artists</h1>}>
+              </Route>
+              <Route path="/top-tracks" element={<h1>Top Tracks</h1>}>
+              </Route>
+              <Route path="/playlists/:id" element={<h1>Playlist ID</h1>}>
+              </Route>
+              <Route path="/playlists" element={<h1>Playlists</h1>}>
+              </Route>
+              <Route path="/" element={
+                <>
+                  <button onClick={logout}>Log Out</button>
 
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar" />
-                )}
-              </div>
-            )}
-          </>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar" />
+                      )}
+                    </div>
+                  )}
+                </>
+              }>
+              </Route>
+            </Routes>
+          </Router>
         )}
       </header>
     </div>
