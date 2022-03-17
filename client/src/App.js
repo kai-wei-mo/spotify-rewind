f32beb77c2b0b18e774a30b085ff1472c8f11e07
Kai Wei Mo <devinkwmo@gmail.com>
2022-03-16 21:40:33 -0400

    Add Profile.js and logout button

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import { catchErrors } from './utils';
 import styled from 'styled-components/macro';
 import { GlobalStyle } from './styles';
 
-import { Login } from './pages';
+import { Login, Profile } from './pages';
 
 const StyledLoginButton = styled.a`
   background-color: var(--green);
@@ -21,6 +21,22 @@ const StyledLoginButton = styled.a`
   margin: 20px;
   border-radius: 30px;
   display: inline-block;
+`;
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
 `;
 
 // Scroll to top of page when changing routes
@@ -58,35 +74,25 @@ function App() {
         {!token ? (
           <Login />
         ) : (
-          <Router>
-            <ScrollToTop />
-            <Routes>
-              <Route path="/top-artists" element={<h1>Top Artists</h1>}>
-              </Route>
-              <Route path="/top-tracks" element={<h1>Top Tracks</h1>}>
-              </Route>
-              <Route path="/playlists/:id" element={<h1>Playlist ID</h1>}>
-              </Route>
-              <Route path="/playlists" element={<h1>Playlists</h1>}>
-              </Route>
-              <Route path="/" element={
-                <>
-                  <button onClick={logout}>Log Out</button>
+          <>
+            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
 
-                  {profile && (
-                    <div>
-                      <h1>{profile.display_name}</h1>
-                      <p>{profile.followers.total} Followers</p>
-                      {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar" />
-                      )}
-                    </div>
-                  )}
-                </>
-              }>
-              </Route>
-            </Routes>
-          </Router>
+            <Router>
+              <ScrollToTop />
+              <Routes>
+                <Route path="/top-artists" element={<h1>Top Artists</h1>}>
+                </Route>
+                <Route path="/top-tracks" element={<h1>Top Tracks</h1>}>
+                </Route>
+                <Route path="/playlists/:id" element={<h1>Playlist ID</h1>}>
+                </Route>
+                <Route path="/playlists" element={<h1>Playlists</h1>}>
+                </Route>
+                <Route path="/" element={<Profile />}>
+                </Route>
+              </Routes>
+            </Router>
+          </>
         )}
       </header>
     </div>
