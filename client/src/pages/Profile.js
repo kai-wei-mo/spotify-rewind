eef61b7f993e3741149201ab83c601140cc38eda
Kai Wei Mo <devinkwmo@gmail.com>
2022-03-21 17:34:11 -0400

    Add top artists endpoint

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile, getCurrentUserPlaylists } from '../spotify';
+import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists } from '../spotify';
 import { StyledHeader } from '../styles';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [playlists, setPlaylists] = useState(null);
+  const [topArtists, setTopArtists] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,6 +15,11 @@ const Profile = () => {
 
       const userPlaylists = await getCurrentUserPlaylists();
       setPlaylists(userPlaylists.data);
+
+      const userTopsArtists = await getTopArtists();
+      setTopArtists(userTopsArtists.data);
+
+      console.log(userTopsArtists.data);
     };
 
     catchErrors(fetchData());
