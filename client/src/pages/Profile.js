352c6e3544513eecfb5a9c92884496b3116c7161
Kai Wei Mo <devinkwmo@gmail.com>
2022-03-22 23:53:03 -0400

    Add track list and top tracks to profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists } from '../spotify';
-
-import { SectionWrapper, ArtistsGrid } from '../components';
+import {
+  getCurrentUserProfile,
+  getCurrentUserPlaylists,
+  getTopArtists,
+  getTopTracks
+} from '../spotify';
+import { SectionWrapper, ArtistsGrid, TrackList } from '../components';
 import { StyledHeader } from '../styles';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [playlists, setPlaylists] = useState(null);
   const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,10 +23,11 @@ const Profile = () => {
       const userPlaylists = await getCurrentUserPlaylists();
       setPlaylists(userPlaylists.data);
 
-      const userTopsArtists = await getTopArtists();
-      setTopArtists(userTopsArtists.data);
+      const userTopArtists = await getTopArtists();
+      setTopArtists(userTopArtists.data);
 
-      console.log(userTopsArtists.data);
+      const userTopTracks = await getTopTracks();
+      setTopTracks(userTopTracks.data);
     };
 
     catchErrors(fetchData());
@@ -50,10 +56,15 @@ const Profile = () => {
               </div>
             </div>
           </StyledHeader>
-          {topArtists && (
+
+          {topArtists && topTracks && (
             <main>
               <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
                 <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+              </SectionWrapper>
+
+              <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
+                <TrackList tracks={topTracks.items.slice(0, 10)} />
               </SectionWrapper>
             </main>
           )}
