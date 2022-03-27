39a87e0632e1c29f7df667ff886af3de966e7eb1
Kai Wei Mo <devinkwmo@gmail.com>
2022-03-27 09:43:19 -0400

    Add StyledDropdown for sorting track metadata

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,5 +1,6 @@
 export { default as GlobalStyle } from './GlobalStyle';
 
+export { default as StyledDropdown } from './StyledDropdown';
 export { default as StyledGrid } from './StyledGrid';
 export { default as StyledHeader } from './StyledHeader';
 export { default as StyledRangeButtons } from './StyledRangeButtons';
